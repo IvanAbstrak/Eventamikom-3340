e6effc4e8ce8f28341227baae1dca37da6d06e90
IvanAbstrak <ivanbagas0404@students.amikom.ac.id>
2026-04-17 13:35:52 +0700

    Tugas 2

--- routes/web.php
+++ routes/web.php
@@ -1,27 +1,44 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\http\Controllers\Homecontroller;
+use App\Http\Controllers\EventController;
+use App\Http\Controllers\Admin\DashboardController;
+use Symfony\Contracts\EventDispatcher\Event;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Rute User Area
+Route::get('/', [Homecontroller::class, 'index'])->name('home');
+Route::get('/event/1', [Eventcontroller::class, 'show'])->name('events.show');
+Route::get('/checkout', [Eventcontroller::class, 'checkout'])->name('checkout');
+Route::get('/my-ticket', [EventController::class, 'ticket'])->name('ticket');
 
-Route::get('/tentang', function () {
-    return '<h1>Ini adalah Halaman Tentang Aplikasi Event Hub</h1>';
+// Rute Admin Area
+Route::group(['prefix'=>'admin','as' => 'admin.'], function() {
+    Route::get('/', [DashboardController::class,'index'])->name('dashbooard');
+    Route::get('/events', [EventController::class,'indexAdmin'])->name('events.index');
 });
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
-Route::get('/kontak', function () {
-    return view('contact');
-});
+// Route::get('/tentang', function () {
+//     return '<h1>Ini adalah Halaman Tentang Aplikasi Event Hub</h1>';
+// });
 
-Route::get('/profil', function () {
-    return view('profil');
-});
+// Route::get('/kontak', function () {
+//     return view('contact');
+// });
 
-Route::get('/katalog', function () {
-    return view('katalog');
-});
+// Route::get('/profil', function () {
+//     return view('profil');
+// });
 
-Route::get('/bantuan', function () {
-    return view('bantuan');
-});
+// Route::get('/katalog', function () {
+//     return view('katalog');
+// });
+
+// Route::get('/bantuan', function () {
+//     return view('bantuan');
+// });
+
+
